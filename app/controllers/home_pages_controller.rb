b9b63fab0574a27cb501ccf85fd6b2822c3ac3a2
orange080 <orange.kouhoku080@gmail.com>
2017-04-03 12:41:15 +0000

    pry

--- app/controllers/home_pages_controller.rb
+++ app/controllers/home_pages_controller.rb
@@ -1,4 +1,7 @@
 class HomePagesController < ApplicationController
   def home
+    if params[:p]
+      redirect_to controller: 'search', action: 'index'
+    end  
   end
 end
--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,14 +1,13 @@
 class SearchController < ApplicationController
 
   def index
-      
-      recipeid = params[:recipeid]
+      # recipeid = params[:p]
       # パラメータを取得する（例 : お肉）
       # category_id = params[:category_id]
       
       # (10) → 肉
       # 本当はここを自由変えられるようにする
-      response = RakutenWebService::Recipe.ranking(recipeid)
+      response = RakutenWebService::Recipe.ranking(params[:p])
       @recipes = response.first(10)
   end
 
